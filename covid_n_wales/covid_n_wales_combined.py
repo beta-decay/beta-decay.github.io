c66972ba176d84a806aa4364447f630840246771
Beta Decay <ckirk9@outlook.com>
2020-05-31 14:06:41 +0100

    automatically added covid graphs

--- covid_n_wales/covid_n_wales_combined.py
+++ covid_n_wales/covid_n_wales_combined.py
@@ -9,14 +9,7 @@ from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
 from matplotlib.figure import figaspect
 import requests
 import re
-"""
-# Download Excel file
-filedata = requests.get('http://www2.nphs.wales.nhs.uk:8080/CommunitySurveillanceDocs.nsf/3dc04669c9e1eaa880257062003b246b/77fdb9a33544aee88025855100300cab/$FILE/Rapid%20COVID-19%20surveillance%20data.xlsx')
 
-# Write to Excel file
-with open('Rapid COVID-19 surveillance data.xlsx', 'wb') as f:
-    f.write(filedata.content)
-"""
 # Set plot style
 plt.style.use("ggplot")
 
