617b874b07d9a0137ee62bab7661ea308f1be211
Beta Decay <ckirk9@outlook.com>
2020-06-02 14:25:11 +0100

    automatically added covid graphs

--- covid_n_wales/update_covid.bat
+++ covid_n_wales/update_covid.bat
@@ -3,6 +3,8 @@ git pull
 python covid_n_wales.py
 python covid_n_wales_separated.py
 python covid_n_wales_combined.py
+python covid_n_wales_separated_tests.py
+python covid_n_wales_tests.py
 
 git add .
 git commit -m "automatically added covid graphs"
