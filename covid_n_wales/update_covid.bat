b68fa2d89ea17c18a75099075653cb6ea0b68597
Beta Decay <ckirk9@outlook.com>
2020-06-03 14:39:55 +0100

    automatically added covid graphs

--- covid_n_wales/update_covid.bat
+++ covid_n_wales/update_covid.bat
@@ -3,8 +3,10 @@ git pull
 python covid_n_wales.py
 python covid_n_wales_separated.py
 python covid_n_wales_combined.py
+python covid_n_wales_combined_tests.py
 python covid_n_wales_separated_tests.py
 python covid_n_wales_tests.py
+python covid_n_wales_combined_raw_tests.py
 python covid_n_wales_separated_raw_tests.py
 python covid_n_wales_raw_tests.py
 
