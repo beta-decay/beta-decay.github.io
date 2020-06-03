b7150a7ae9e83586ce67745c3a2f2035bbb14895
Beta Decay <ckirk9@outlook.com>
2020-06-03 14:42:24 +0100

    automatically added covid graphs

--- covid_n_wales/covid_n_wales_combined_tests.py
+++ covid_n_wales/covid_n_wales_combined_tests.py
@@ -32,7 +32,7 @@ df = xl.parse("Tests by specimen date")
 # List of counties in North Wales
 county_list = ["Conwy", "Denbighshire", "Flintshire", "Gwynedd", "Isle of Anglesey", "Wrexham"]
 
-fig = plt.figure(dpi=1000)
+fig = plt.figure(dpi=400)
 w, h = figaspect(0.8)
 plt.subplots(figsize=(w,h))
 plt.subplot(1,1,1)
@@ -100,7 +100,7 @@ plt.xlabel("Date")
 
 # Save figure to png file
 plt.tight_layout()
-plt.savefig("covid_n_wales_combined_tests.png",dpi=1000)
+plt.savefig("covid_n_wales_combined_tests.png",dpi=400)
 
 # Get first sheet
 contents = xl.parse("Contents")
