7a25887cc8524be68b21d7f3eac163cea3437fae
= <=>
2021-06-05 19:33:32 +0100

    automatically added covid graphs

--- covid_n_wales/covid_n_wales.py
+++ covid_n_wales/covid_n_wales.py
@@ -11,7 +11,7 @@ import requests
 import re
 
 # Download Excel file
-filedata = requests.get('http://www2.nphs.wales.nhs.uk:8080/CommunitySurveillanceDocs.nsf/3dc04669c9e1eaa880257062003b246b/77fdb9a33544aee88025855100300cab/$FILE/Rapid%20COVID-19%20surveillance%20data.xlsx')
+filedata = requests.get('http://www2.nphs.wales.nhs.uk:8080/CommunitySurveillanceDocs.nsf/3dc04669c9e1eaa880257062003b246b/c84f742604ce56f0802586b600374b49/$FILE/Rapid%20COVID-19%20surveillance%20data.xlsx')
 
 # Write to Excel file
 with open('Rapid COVID-19 surveillance data.xlsx', 'wb') as f:
